0f40f769f85ad446457654673813a790cd49e652
Shuofeng Wang <shuofengwang666@gmail.com>
2020-06-23 23:34:56 -0400

    Initial Commit for this fork.

---
+++ src/components/subscriptionCard.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SubscriptionCard from './subscriptionCard';
+
+export default{
+    component: SubscriptionCard,
+    title: 'Subscription Card'
+};
+
+// const subscriptionsArray = [
+//     {newsletterName: "Ask Leo", likedNumber: "4", savedNumber: "6", receivingEmail: 'work email', elements: 3,
+//         titles: ['How do I Back Up my Computer?', 'How do I remove malware?', 'Dealing with browser problems'], SubscriptionCarddates: ['June 9th, 2020 10:37AM', 'June 8th, 2020 10:37AM', 'June 7th, 2020 10:37AM'],
+//         activity: ['Sent by source', 'Read by me', 'Sent by source'], dates: ['January 22, 2019, 4:08 PM']},
+//     {newsletterName: "Harvard Business Review", likedNumber: "4", savedNumber: "6", receivingEmail: 'work email',
+//         titles: ['How do I Back Up my Computer?', 'How do I remove malware?', 'Dealing with browser problems'],  SubscriptionCarddates: ['June 9th, 2020 10:37AM', 'June 8th, 2020 10:37AM', 'June 7th, 2020 10:37AM'],
+//         activity: ['Sent by source', 'Read by me', 'Sent by source'], dates: ['June 15, 2020, 6:09 PM']},
+//     {newsletterName: "The Skim", likedNumber: "8", savedNumber: "6", receivingEmail: 'work email', elements: 3,
+//         titles: ['How do I Back Up my Computer?', 'How do I remove malware?', 'Dealing with browser problems'],  SubscriptionCarddates: ['June 9th, 2020 10:37AM', 'June 8th, 2020 10:37AM', 'June 7th, 2020 10:37AM'],
+//         activity: ['Sent by source', 'Read by me', 'Sent by source'], dates: ['April 23, 2020, 4:03 PM']},
+//     {newsletterName: "The news times", likedNumber: "20", savedNumber: "6", receivingEmail: 'work email', elements: 3,
+//         titles: ['How do I Back Up my Computer?', 'How do I remove malware?', 'Dealing with browser problems'],  SubscriptionCarddates: ['June 9th, 2020 10:37AM', 'June 8th, 2020 10:37AM', 'June 7th, 2020 10:37AM'],
+//         activity: ['Sent by source', 'Read by me', 'Sent by source'], dates: ['March 23, 2019, 9:15 PM']},
+// ];
+
+export const Default = () => <SubscriptionCard/>;
